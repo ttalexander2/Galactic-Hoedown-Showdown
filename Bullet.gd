06106f6cf9b6f5e3845500392ecca6a9604ed78d
Cameron B Larson <tamick88@gmail.com>
2020-10-23 23:07:58 -0700

    Player bullet system added.

---
+++ Bullet.gd
@@ -0,0 +1,26 @@
+extends Sprite
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var direction = Vector2.ZERO
+const SPEED = 1000;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func set_direction(x):
+	direction.x = x
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#direction = direction.move_toward(direction * SPEED, SPEED * delta)
+	pass
+
+func _physics_process(delta):
+	position.x += direction.x * SPEED * delta
+
+func destroy():
+	queue_free()
